ff5596565e5c41c636e4d9d71bff726cb5fa6fc9
0w4n <jorgelevia@gmail.com>
2023-03-26 20:23:29 +0200

    cargar el proyecto

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-$(document).ready(function(){
+//colores
+const azul = rgb(60, 119, 220);
+const rojo = rgb(229, 58, 98);
 
-    var height = $(window).height();
+// numeros
+var numero1 = document.getElementById("numero-1");
+var numero2 = document.getElementById("numero-2");
+var numero3 = document.getElementById("numero-3");
+var numero4 = document.getElementById("numero-4");
+var numfin = document.getElementById("num-fin");
 
-    $('#body-wrapped').height(height);
+numero1.addEventListener("DOMCharacterModificated", function mirarnum(rojo, azul) {
+    if (numero1 == "1") {
+        numero1.style.color = rojo;
+    } else {
+        numero1.style.color = azul;
+    }    
+})
+numero2.addEventListener("DOMCharacterModificated", function mirarnum(rojo, azul) {
+    if (numero2 == "1") {
+        numero2.style.color = rojo;
+    } else {
+        numero2.style.color = azul;
+    }    
+})
+numero3.addEventListener("DOMCharacterModificated", function mirarnum(rojo, azul) {
+    if (numero3 == "1") {
+        numero3.style.color = rojo;
+    } else {
+        numero3.style.color = azul;
+    }    
+})
+numero4.addEventListener("DOMCharacterModificated", function mirarnum(rojo, azul) {
+    if (numero4 == "1") {
+        numero4.style.color = rojo;
+    } else {
+        numero4.style.color = azul;
+    }    
+})
+
+
+$(document).ready(function () {
+  var height = $(window).height();
+
+  $("#body-wrapped").height(height);
 });
