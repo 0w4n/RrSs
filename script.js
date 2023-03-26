57098baa4094cf4c475a44e08c7fa73014f5e3fd
0w4n <jorgelevia@gmail.com>
2023-03-26 22:22:39 +0200

    cargar el proyecto

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 //colores
-const azul = rgb(60, 119, 220);
-const rojo = rgb(229, 58, 98);
+const azul = "#3C77DC";
+const rojo = "#E53A62";
 
 // numeros
-var numero1 = document.getElementById("numero-1");
-var numero2 = document.getElementById("numero-2");
-var numero3 = document.getElementById("numero-3");
-var numero4 = document.getElementById("numero-4");
+var numero1 = document.getElementById("inumero-1").textContent;
+var numero2 = document.getElementById("inumero-2").textContent;
+var numero3 = document.getElementById("inumero-3").textContent;
+var numero4 = document.getElementById("inumero-4").textContent;
 var numfin = document.getElementById("num-fin");
+
+numero1.textContent;
 
 numero1.addEventListener("DOMCharacterModificated", function mirarnum(rojo, azul) {
     if (numero1 == "1") {
@@ -39,8 +41,8 @@ numero4.addEventListener("DOMCharacterModificated", function mirarnum(rojo, azul
 })
 
 
-$(document).ready(function () {
-  var height = $(window).height();
+// $(document).ready(function () {
+//   var height = $(window).height();
 
-  $("#body-wrapped").height(height);
-});
+//   $("#body-wrapped").height(height);
+// });
